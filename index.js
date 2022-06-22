4c0a31c7db09295eee692de856ffbbba2e67f9bb
AlievIB <020alievib@gmail.com>
2022-06-22 12:38:41 +0300

    initial server

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes"));
+
+mongoose
+  .connect(process.env.MONGO_SERVER)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Server started successfully on http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch((e) => console.log("Error connecting to MongoDB server" + e.message));
